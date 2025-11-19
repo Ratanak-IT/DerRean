af1f78fc873dd962f769d610d98d9d64b8328c18
ratanak <ratanak1intel@gmail.com>
2025-11-19 08:39:11 +0700

    dashboard add

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return(
+        <>
+                <h1 className="flex justify-center items-center min-h-screen dark:text-blue-300">This is wishlist page</h1>
+
+        </>
+    )
+}
